04406a150e87ae7ab25fe8c3f8586393bbc3f430
postwo <codo7717@naver.com>
2024-11-24 12:54:37 +0900

    모듈 추가 (jpaboard-admin)

---
+++ JpaBoard-admin/src/main/java/com/example/JpaBoard_admin/JpaBoardAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.JpaBoard_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardAdminApplication.class, args);
+	}
+
+}
---
+++ JpaBoard-admin/src/test/java/com/example/JpaBoard_admin/JpaBoardAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JpaBoard_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
