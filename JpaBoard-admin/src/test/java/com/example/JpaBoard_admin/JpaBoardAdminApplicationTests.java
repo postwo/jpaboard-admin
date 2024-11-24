a8de2371c61fea7642614e29185a9f232fad87fb
postwo <codo7717@naver.com>
2024-11-24 13:52:41 +0900

    yml 주석 수정과 빌들할때 test실패하는걸 방지하기 추가

--- JpaBoard-admin/src/test/java/com/example/JpaBoard_admin/JpaBoardAdminApplicationTests.java
+++ JpaBoard-admin/src/test/java/com/example/JpaBoard_admin/JpaBoardAdminApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.JpaBoard_admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test") //yml에 있는 testdb를 사용하겠다는 뜻이다 이렇게 설정함으로써 test실패를 예방 할 수 있다 ==Configuration Processor
 @SpringBootTest
 class JpaBoardAdminApplicationTests {
 
