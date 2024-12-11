ccb482c3ef530a9215148937e8457970834990ef
postwo <codo7717@naver.com>
2024-12-11 21:32:31 +0900

    delete 테스트코드가 동작이 안돼서     @BeforeEach     public void setUp() 을 추가해서 해결

--- JpaBoard-admin/src/test/java/com/example/JpaBoard_admin/repository/UserAccountRepositoryTest.java
+++ JpaBoard-admin/src/test/java/com/example/JpaBoard_admin/repository/UserAccountRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.JpaBoard_admin.repository;
 
 import com.example.JpaBoard_admin.domain.UserAccount;
 import com.example.JpaBoard_admin.domain.constant.RoleType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -27,6 +29,20 @@ class JpaRepositoryTest {
     public JpaRepositoryTest(@Autowired UserAccountRepository userAccountRepository) {
         this.userAccountRepository = userAccountRepository;
     }
+
+    //이렇게 하면 delete 테스트는 통과 된다.
+    @BeforeEach
+    public void setUp() {
+        userAccountRepository.deleteAll();
+
+        // 테스트 데이터 삽입
+        userAccountRepository.save(UserAccount.of("uno", "{noop}asdf1234", new HashSet<>(Set.of(RoleType.ADMIN)), "Uno", "uno@mail.com", "I am Uno."));
+        userAccountRepository.save(UserAccount.of("mark", "{noop}asdf1234", new HashSet<>(Set.of(RoleType.MANAGER)), "Mark", "mark@mail.com", "I am Mark."));
+        userAccountRepository.save(UserAccount.of("susan", "{noop}asdf1234", new HashSet<>(Set.of(RoleType.MANAGER, RoleType.DEVELOPER)), "Susan", "susan@mail.com", "I am Susan."));
+        userAccountRepository.save(UserAccount.of("jim", "{noop}asdf1234", new HashSet<>(Set.of(RoleType.USER)), "Jim", "jim@mail.com", "I am Jim."));
+    }
+
+
 
     @DisplayName("회원 정보 select 테스트")
     @Test
@@ -56,6 +72,7 @@ class JpaRepositoryTest {
         assertThat(userAccountRepository.count()).isEqualTo(previousCount + 1);
     }
 
+    //rollback mode에 트랜잭션이 걸린 상태이다. 업데이트가 반영이 안되게 한다.
     @DisplayName("회원 정보 update 테스트")
     @Test
     void givenUserAccountAndRoleType_whenUpdating_thenWorksFine() {
@@ -78,7 +95,7 @@ class JpaRepositoryTest {
     @Test
     void givenUserAccount_whenDeleting_thenWorksFine() {
         // Given
-        long previousCount = userAccountRepository.count();
+        long previousCount = userAccountRepository.count(); //지웠으니까 하나가 줄어든다.
         UserAccount userAccount = userAccountRepository.getReferenceById("uno");
 
         // When
