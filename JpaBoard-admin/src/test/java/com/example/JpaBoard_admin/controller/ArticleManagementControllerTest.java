548f63e0d95afe3fbdca557be26cdfb7ea5a229e
postwo <codo7717@naver.com>
2024-12-27 14:11:21 +0900

    # 리팩토링: 메소드명, 테스트 제목 등 다듬기

--- JpaBoard-admin/src/test/java/com/example/JpaBoard_admin/controller/ArticleManagementControllerTest.java
+++ JpaBoard-admin/src/test/java/com/example/JpaBoard_admin/controller/ArticleManagementControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@DisplayName("View 컨트롤러 - 게시글 관리")
+@DisplayName("컨트롤러 - 게시글 관리")
 @Import(SecurityConfig.class)
 @WebMvcTest(ArticleManagementController.class)
 class ArticleManagementControllerTest {
@@ -38,7 +38,7 @@ class ArticleManagementControllerTest {
         this.mvc = mvc;
     }
 
-    @DisplayName("[view][GET] 게시글 관리 페이지 - 정상 호출")
+    @DisplayName("[view][POST] 게시글 삭제 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleManagementView_thenReturnsArticleManagementView() throws Exception {
         // Given
