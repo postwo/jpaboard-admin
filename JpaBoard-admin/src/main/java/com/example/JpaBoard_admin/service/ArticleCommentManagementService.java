7f9e19c437f3eb9f715a4848e11ec0fb786c1fb3
postwo <codo7717@naver.com>
2024-12-27 14:24:46 +0900

    # 댓글 관리 서비스의 비즈니스 로직 및 컨트롤러 테스트 작성

---
+++ JpaBoard-admin/src/main/java/com/example/JpaBoard_admin/service/ArticleCommentManagementService.java
@@ -0,0 +1,22 @@
+package com.example.JpaBoard_admin.service;
+
+import com.example.JpaBoard_admin.dto.ArticleCommentDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleCommentManagementService {
+    public List<ArticleCommentDto> getArticleComments() {
+        return List.of();
+    }
+
+    public ArticleCommentDto getArticleComment(Long articleCommentId) {
+        return null;
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+    }
+}
