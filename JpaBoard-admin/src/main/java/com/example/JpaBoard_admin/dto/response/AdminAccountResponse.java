ae105c907ecd3abfb2e8283087c05957001c6b63
postwo <codo7717@naver.com>
2025-01-03 14:58:23 +0900

    # 어드민 회원 api 의 응답 객체 설계

---
+++ JpaBoard-admin/src/main/java/com/example/JpaBoard_admin/dto/response/AdminAccountResponse.java
@@ -0,0 +1,36 @@
+package com.example.JpaBoard_admin.dto.response;
+
+import com.example.JpaBoard_admin.domain.constant.RoleType;
+import com.example.JpaBoard_admin.dto.AdminAccountDto;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+public record AdminAccountResponse(
+        String userId,
+        String roleTypes,
+        String email,
+        String nickname,
+        String memo,
+        LocalDateTime createdAt,
+        String createdBy
+) {
+
+    public static AdminAccountResponse of(String userId, String roleTypes, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy) {
+        return new AdminAccountResponse(userId, roleTypes, email, nickname, memo, createdAt, createdBy);
+    }
+
+    public static AdminAccountResponse from(AdminAccountDto dto) {
+        return AdminAccountResponse.of(
+                dto.userId(),
+                dto.roleTypes().stream()
+                        .map(RoleType::getDescription)
+                        .collect(Collectors.joining(", ")),
+                dto.email(),
+                dto.nickname(),
+                dto.memo(),
+                dto.createdAt(),
+                dto.createdBy()
+        );
+    }
+}
