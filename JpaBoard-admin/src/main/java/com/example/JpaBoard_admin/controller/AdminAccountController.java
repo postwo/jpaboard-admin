c775eae194b7cbcbe07448905ec876c453d08b68
postwo <codo7717@naver.com>
2025-01-03 15:00:55 +0900

    # 어드민 회원 컨트롤러에 필요한 핸들러 메소드 및 테스트

--- JpaBoard-admin/src/main/java/com/example/JpaBoard_admin/controller/AdminAccountController.java
+++ JpaBoard-admin/src/main/java/com/example/JpaBoard_admin/controller/AdminAccountController.java
@@ -1,24 +1,34 @@
 package com.example.JpaBoard_admin.controller;
 
+import com.example.JpaBoard_admin.dto.response.AdminAccountResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/admin/members")
 @Controller
 public class AdminAccountController {
+
     @GetMapping
-    public String members(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            Model model, HttpServletRequest request
-    ) {
-        // requestURI를 모델에 추가
+    public String members(Model model, HttpServletRequest request) {
+        // requestURI를 모델에 추가 이걸 추가 안해주면 thymeleaf 문법하고 맞지 않아서 에러가 뜬다.
         model.addAttribute("currentURI", request.getRequestURI());
         return "admin/members";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/admin/members")
+    public List<AdminAccountResponse> getMembers() {
+        return List.of();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    @DeleteMapping("/api/admin/members/{userId}")
+    public void delete(@PathVariable String userId) {
     }
 }
