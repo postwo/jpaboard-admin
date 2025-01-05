074b84bede16958749e4913dde258abfc4d65371
postwo <codo7717@naver.com>
2025-01-05 11:35:06 +0900

    # 회원 관리 기능 컨트롤러, 서비스, 뷰 구현

--- JpaBoard-admin/src/main/java/com/example/JpaBoard_admin/controller/UserAccountManagementController.java
+++ JpaBoard-admin/src/main/java/com/example/JpaBoard_admin/controller/UserAccountManagementController.java
@@ -1,24 +1,43 @@
 package com.example.JpaBoard_admin.controller;
 
+import com.example.JpaBoard_admin.service.UserAccountManagementService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
 @Controller
 public class UserAccountManagementController {
+
+    private final UserAccountManagementService userAccountManagementService;
+
     @GetMapping
     public String userAccounts(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             Model model, HttpServletRequest request
     ) {
+        model.addAttribute(
+                "userAccounts",
+                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
+        );
+
         // requestURI를 모델에 추가
         model.addAttribute("currentURI", request.getRequestURI());
+
         return "management/user-accounts";
+    }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserAccountResponse userAccount(@PathVariable String userId) {
+        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+    }
+
+    @PostMapping("/{userId}")
+    public String deleteUserAccount(@PathVariable String userId) {
+        userAccountManagementService.deleteUserAccount(userId);
+        return "redirect:/management/user-accounts";
     }
 }
